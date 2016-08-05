ddda0e8339a2ad72cfbc864ed8b97d182b96be0e
Tommy <TommyCohn116@gmail.com>
2016-08-05 12:52:01 -0400

    Just open the page to try it. Big numbers will make the webpage sad.

---
+++ script.js
@@ -0,0 +1,16 @@
+var inputM = prompt("M = ?");
+var inputN = prompt("N = ?");
+
+function ackermann(m, n) {
+	if(m == 0) {
+		return n + 1;
+	}
+	else if(m > 0 && n == 0) {
+		return ackermann(m-1, 1);
+	}
+	else if(m > 0 && n > 0) {
+		return ackermann(m-1, ackermann(m, n-1));
+	}
+}
+
+alert(ackermann(inputM, inputN));
